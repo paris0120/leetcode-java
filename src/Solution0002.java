c51763efb20ddc14d9b4aa258b9397222bbea61c
unknown <paris0120@gmail.com>
2022-05-04 21:00:37 -0400

    clear

--- src/Solution0002.java
+++ src/Solution0002.java
@@ -1,2 +1,55 @@
+/**
+ * 2. Add Two Numbers
+ * tc: O(n)
+ * sc: O(n)
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 public class Solution0002 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode output = new ListNode(0);
+        ListNode point = output;
+        boolean lead = false;
+        while(l1!=null || l2!=null) {
+            point.next = new ListNode(0);
+            point = point.next;
+            if(l1!=null) {
+                point.val+=l1.val;
+                l1 = l1.next;
+            }
+            if(l2!=null) {
+                point.val+=l2.val;
+                l2 = l2.next;
+            }
+            if(lead){
+                lead = false;
+                point.val++;
+            }
+            if(point.val>=10) {
+                lead = true;
+                point.val-=10;
+            }
+        }
+        if(lead) {
+            point.next = new ListNode(1);
+        }
+        return output.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
 }
--- src/Solution0225.java
+++ src/Solution0225.java
@@ -1,2 +1,45 @@
+fdsa/**
+ * 225. Implement Stack using Queues
+ */
 public class Solution0225 {
+    Queue<Integer> q;
+    int top;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q.add(x);
+        top = x;
+    }
+
+    public int pop() {
+        Queue<Integer> tmp = new LinkedList<>();
+        while(q.size()>1){
+            top = q.remove();
+            tmp.add(top);
+        }
+        int output = q.remove();
+        q = tmp;
+        return output;
+    }
+
+    public int top() {
+        return top;
+    }
+
+
+    public boolean empty() {
+        return q.size()==0;
+    }
 }
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
