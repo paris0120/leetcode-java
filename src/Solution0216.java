6ffd1cb143759e9389f4ffc1c0b435f1d0503e24
unknown <paris0120@gmail.com>
2022-05-10 13:09:53 -0400

    1679

--- src/Solution0216.java
+++ src/Solution0216.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Solution0216 {
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 216. Combination Sum III
+ * tc: O(1)
+ * sc: O(1)
+ */
+public class Solution0216 {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        return get(k, n, 9);
+
+    }
+
+    public List<List<Integer>> get(int k, int n, int max) {
+        List<List<Integer>> output = new ArrayList<List<Integer>>();
+        if(k == 1) {
+            if(max>=n && n>=1) {
+                List<Integer> o = new ArrayList<Integer>();
+                o.add(n);
+                output.add(o);
+            }
+        }
+        else for(int i = k;i<=max;i++) {
+            List<List<Integer>> t = get(k-1, n-i, i-1);
+            for(List<Integer> l:t) {
+                l.add(i);
+                output.add(l);
+            }
+        }
+        return output;
+    }
 }
--- src/Solution1679.java
+++ src/Solution1679.java
@@ -1,2 +1,24 @@
+import java.util.Arrays;
+
+/**
+ * 1679. Max Number of K-Sum Pairs
+ * tc:O(log(n)n)
+ * sc:O(n)
+ */
 public class Solution1679 {
+    public int maxOperations(int[] nums, int k) {
+        int i = 0;
+        Arrays.sort(nums);
+        int b = 0, e = nums.length - 1;
+        while(b<e) {
+            if(nums[b]+nums[e]>k) e--;
+            else if (nums[b]+nums[e]<k) b++;
+            else {
+                b++;
+                e--;
+                i++;
+            }
+        }
+        return i;
+    }
 }
